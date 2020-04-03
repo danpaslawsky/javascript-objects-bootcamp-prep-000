14c9faaaa6edc4116e1f1cc93c50702b89a651e2
danpaslawsky <danpaslawsky@gmail.com>
2020-04-03 18:28:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,9 @@ var playlist = {
   Tupac:"All Eyes"
 };
 
-function updatePlaylist(playlist, artistName, songTitle)
+function updatePlaylist(playlist, artistName, songTitle){
+  Object.assign({}, playlist, {artistName: songTitle});
+}
 
 
 
